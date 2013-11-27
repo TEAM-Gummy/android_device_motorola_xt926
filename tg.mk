9f7d5182e4c1150a6204dca6c04e9c85072ab464
dhacker29 <davidhackerdvm@gmail.com>
2013-11-27 04:16:41 -0600

    Gummify
    Change-Id: I272318b1b39a9ca014b9e7c1466cdb5828239c32

--- tg.mk
+++ tg.mk
@@ -1,8 +1,8 @@
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Gummy stuff.
+$(call inherit-product, vendor/Gummy/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/Gummy/config/nfc_enhanced.mk)
 
 # Boot animation
 TARGET_SCREEN_WIDTH := 720
@@ -10,7 +10,7 @@ TARGET_SCREEN_HEIGHT := 1280
 
 # Release name
 PRODUCT_RELEASE_NAME := DROID RAZR HD
-PRODUCT_NAME := cm_xt926
+PRODUCT_NAME := tg_xt926
 
 $(call inherit-product, device/motorola/xt926/full_xt926.mk)
 
